912c0db0cebe473b714d043654bee7a260f54055
Shoomankhatri <shoomankhatri@gmail.com>
2024-01-19 21:31:46 +0545

    css added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ export default function Home(){
   return (
     <div className="max-w-4xl bg-orange-400 mx-auto p-5">
         <h1 className="text-center text-[40px]">Todo App</h1>
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-4">
           <input type="text" placeholder="write movie name" className="w-[80%]" />
-          <button className="bg-blue-500 w-[20%]">Add Movie</button>
+          <button className="bg-blue-500 w-[20%] text-white">Add Movie</button>
         </div>
     </div>
   )
