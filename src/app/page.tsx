0fac4aecd869c166d1a68a47257456a3947e5479
Shoomankhatri <shoomankhatri@gmail.com>
2024-01-27 13:01:55 +0545

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       setTodos([...newArray, { movie: inputVal, id: id }]);
       setInput("");
       setId(0);
-      return
+      return;
     }
     setTodos([...todos, { movie: inputVal, id: id }]);
     setInput("");
@@ -37,9 +37,18 @@ export default function Home() {
     console.log(obj);
   };
 
+  const delItem = (id: any) => {
+    let newArray = todos.filter((item) => item.id !== id);
+    setTodos([...newArray]);
+    // console.log(newArray);
+  };
+
   return (
-    <div className="max-w-4xl bg-orange-400 mx-auto p-5">
-      <h1 className="text-center text-[40px] underline">Todo App</h1>
+    <div
+      className="max-w-4xl  mx-auto p-5"
+      style={{ color: "#D63484", background: "#402B3A" }}
+    >
+      <h1 className="text-center text-[40px] ">TodoList App</h1>
 
       {/* start input div  */}
       <div className="flex justify-between gap-4 mt-5">
@@ -59,7 +68,7 @@ export default function Home() {
         />
         <button
           onClick={addItem}
-          className="bg-blue-500 w-[20%] text-white p-2 rounded hover:bg-blue-400"
+          className="bg-pink-700 w-[20%] text-white p-2 rounded hover:bg-pink-600"
         >
           Add Movie
         </button>
@@ -75,18 +84,21 @@ export default function Home() {
 
         {todos.map((item: any, i: any) => {
           return (
-            <div className="shadow p-4" key={i}>
+            <div className="shadow p-4" key={i} style={{background:"#FF9BD2",borderRadius:"10px"}}>
               <div className="flex justify-between text-lg">
                 <span className="shadow rounded-r-full h-8 w-8 text-center my-auto">
                   {i + 1}
                 </span>
-                <span className="shadow rounded-r-full h-8 w-8 text-center my-auto cursor-pointer text-red-500">
+                <span
+                  onClick={() => delItem(item.id)}
+                  className="shadow rounded-r-full h-8 w-8 text-center my-auto cursor-pointer text-red-700"
+                >
                   X
                 </span>
               </div>
 
               {/* data div */}
-              <div className="mt-5 text-[30px] text-gray-700">{item.movie}</div>
+              <div className="mt-5 text-[30px]" style={{color:"#FF9BD2"}}>{item.movie}</div>
               <div
                 onClick={() => {
                   editItem(item.id);
